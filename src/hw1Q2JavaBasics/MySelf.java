641d3be487845b16f623d4cb92ea40acf5b353b1
Ruhul-Kuddus <mdrhlkdus@gmail.com>
2022-01-16 23:19:45 -0500

    Jan 16 2022 11:17pm

---
+++ src/hw1Q2JavaBasics/MySelf.java
@@ -0,0 +1,14 @@
+package hw1Q2JavaBasics;
+
+/*Projects names are generally starts with UpperCase.
+ * Class names are generally starts with UpperCase.
+ * Package names are generally starts with lowerCase.
+ */
+
+public class MySelf {
+//A main method in java is an entry point to start the execution of a program.
+	public static void main(String[] args) {
+
+		System.out.println("Ruhul");
+	}
+}
---
+++ src/hw2Q2JavaBasics/MyCar.java
@@ -0,0 +1,22 @@
+package hw2Q2JavaBasics;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		
+		System.out.println("my name is Ruhul Kuddus, i love to drive my car, its toyota rav4 2017, black color all wheel drive");
+		System.out.println("my name is Ruhul Kuddus, " + "i love to drive my car, " + "its toyota rav4 2017, "+ "black color all wheel drive");
+		System.out.println("my name is Ruhul Kuddus, i love to drive my car, its toyota rav4 2017, black color all wheel drive");
+		
+		
+		System.out.println("MY car color is black");
+		System.out.println("its make toyota rav4");
+		System.out.println("2017");
+		System.out.println("all wheel drive");
+		System.out.println("My car color is black");
+		
+		
+
+	}
+
+}
